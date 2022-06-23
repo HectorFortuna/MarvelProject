cb23178ffcecb16b3c7a9d3de4f5228baa784ce1
Hector Fortuna <hectorfortuna@brq.com>
2022-06-23 18:55:09 -0300

    Criação do banco de dados e do DAO

---
+++ app/src/main/java/com/hectorfortuna/marvelproject/data/db/CharacterDAO.kt
@@ -0,0 +1,18 @@
+package com.hectorfortuna.marvelproject.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.hectorfortuna.marvelproject.data.model.Results
+
+@Dao
+interface CharacterDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCharacter(result: Results)
+
+    @Query("SELECT * FROM results_table")
+    fun getAllCharacters(): LiveData<List<Results>>
+
+    @Delete
+    suspend fun deleteCharacter(result: Results)
+}
--- app/src/main/java/com/hectorfortuna/marvelproject/data/model/Results.kt
+++ app/src/main/java/com/hectorfortuna/marvelproject/data/model/Results.kt
@@ -1,9 +1,13 @@
 package com.hectorfortuna.marvelproject.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
+@Entity(tableName = "results_table")
 data class Results(
+    @PrimaryKey
     @SerializedName("id")
     val id: Long,
     @SerializedName("name")
