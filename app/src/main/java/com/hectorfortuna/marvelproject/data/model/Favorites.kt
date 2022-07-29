1fdaeae4f7e15a670ff9b52e39f981f500abfbfe
liebersonsantos <T3637808@timbrasil.com.br>
2022-07-29 12:41:55 -0300

    adjustment

---
+++ app/src/main/java/com/hectorfortuna/marvelproject/data/model/Favorites.kt
@@ -0,0 +1,34 @@
+package com.hectorfortuna.marvelproject.data.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+@Entity(tableName = "favorites_table", primaryKeys = ["id", "email"])
+@Parcelize
+data class Favorites(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("modified")
+    val modified: String,
+    @SerializedName("thumbnail")
+    val thumbnail: Thumbnail,
+    @SerializedName("resourceURI")
+    val resourceURI: String,
+    @SerializedName("comics")
+    val comics: Comics,
+    @SerializedName("series")
+    val series: Series,
+    @SerializedName("stories")
+    val stories: Stories,
+    @SerializedName("events")
+    val events: Events,
+    @SerializedName("urls")
+    val urls: List<Urls>,
+    val email: String
+) : Parcelable
